6d54590895ad848accd91e70114897087798e0f5
James Williams <jamesrwilliams91@gmail.com>
2015-06-19 08:48:16 -0700

    added assets to public folder as well as project images, added models to routes

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,10 +1,8 @@
 import DS from 'ember-data';
 
-var Answer = DS.Model.extend({
+export default DS.Model.extend({
   name: DS.attr(),
   body: DS.attr(),
   question: DS.belongsTo('question', {async:true})
 
 });
-
-export default Answer;
--- app/models/question.js
+++ app/models/question.js
@@ -1,10 +1,8 @@
 import DS from 'ember-data';
 
-var Question = DS.Model.extend({
+export default DS.Model.extend({
   author: DS.attr(),
   question: DS.attr(),
   description: DS.attr(),
   answers: DS.hasMany('answer', {async:true})
 });
-
-export default Question;
